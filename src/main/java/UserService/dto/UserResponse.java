2a22b2dd723b9c0a3c30f2413cd217925ea3e3a0
0legp <0legpronko@gmail.com>
2026-02-03 23:08:56 +0300

    add config

--- src/main/java/UserService/dto/UserResponse.java
+++ src/main/java/UserService/dto/UserResponse.java
@@ -1,20 +1,19 @@
 package UserService.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class UserResponse {
     private Long id;
     private String name;
     private String email;
     private Integer age;
+
 
 }
